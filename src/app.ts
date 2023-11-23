5e13e6ec0535aca898ac1e6098d5514b1a8d4cce
Merajul Islam <meraj.exe@gmail.com>
2023-11-23 15:28:49 +0600

    user routes and interface created

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import { UserRoutes } from './app/modules/user/user.routes';
 
 const app: Application = express();
 
@@ -7,8 +8,24 @@ const app: Application = express();
 app.use(express.json());
 app.use(cors());
 
+// setting routes of api to user routes
+app.use('/api', UserRoutes);
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome api is running');
+});
+
+// response for invalid url
+
+app.all('*', (req: Request, res: Response) => {
+  res.json({
+    success: false,
+    message: 'Invalid url',
+    error: {
+      code: 404,
+      description: 'Sorry the url is not valid',
+    },
+  });
 });
 
 export default app;
